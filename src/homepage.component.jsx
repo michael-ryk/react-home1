621b949ff01331d4105a43ffe14caffdaad1057f
MichaelR <spider747@gmail.com>
2021-07-10 17:05:20 +0300

    First commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+// Functional component
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Stones</h1>
+                <span className='subtitle'>Buy now</span>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
